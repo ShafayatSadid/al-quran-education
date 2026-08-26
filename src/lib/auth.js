9ddee425a83cb17ab620a5305fceac28c8836483
ShafayatSadid <mdshafayathossainsadid@gmail.com>
2026-08-26 20:43:08 +0300

    fixed all error

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,15 +2,14 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
-console.log('client:', client);
-const db = client.db("al-quran-education");
+
+const client = new MongoClient(`${process.env.MONGODB_URI}`);
+const db = client.db('al-quran-server');
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    client
-  }),
+  database: mongodbAdapter(db, { client }),
   emailAndPassword: {
     enabled: true
   },
+
 });
